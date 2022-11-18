547dcd08b17e202931cf237d4e0cb6d595c469a5
chystarr <chynnastarr2@gmail.com>
2022-11-18 13:23:37 -0500

    Made search bar its own component

--- client/src/pages/ClassSearchPage.js
+++ client/src/pages/ClassSearchPage.js
@@ -1,24 +1,30 @@
 import React from "react";
 import ClassCard from "../components/ClassCard";
 
+function ClassSearchBar() {
+  return(
+    <div className="col-10 col-md-8 col-lg-7 mx-auto">
+      <form>
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="Enter a class name"
+            className="form-control"
+            autoFocus
+          />
+          <button type="submit" className="btn btn-primary">
+            Search
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function ClassSearchPage() {
   return(
       <div>
-        <div className="col-10 col-md-8 col-lg-7 mx-auto">
-          <form>
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Enter a class name"
-                className="form-control"
-                autoFocus
-              />
-              <button type="submit" className="btn btn-primary">
-                Search
-              </button>
-            </div>
-          </form>
-        </div>
+        <ClassSearchBar />
         <ClassCard id={1}/>
       </div>
   );
