a5022e209592ac9ae4759557883531af6646fcfb
chystarr <chynnastarr2@gmail.com>
2022-11-18 13:53:36 -0500

    Add class component to My Classes page (mainly for demonstrative purposes)

--- client/src/pages/MyClassesPage.js
+++ client/src/pages/MyClassesPage.js
@@ -1,10 +1,14 @@
 import React from "react";
+import ClassCard from "../components/ClassCard";
 
+// should show a different version of class cards here, without the "Add Class" option
+// or just don't have that option outside of the class details page?
 function MyClassesPage() {
   return(
       <div>
         <p>This is the My Classes page</p>
         <p>Classes the user is enrolled in will be displayed here</p>
+        <ClassCard id={1}/>
       </div>
   );
 }
