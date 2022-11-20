559af56bb293ffae4b0c69cabbd6520b66edcc10
jjames003 <jjames003@citymail.cuny.edu>
2022-11-20 01:03:31 -0500

    Added features on Home Page- Two Buttons; Added styles to App.css- Joel

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { useNavigate, useLocation } from "react-router-dom";
 
 
 
 function HomePage() {
+  const navigate = useNavigate();
+  const navigateToAboutUs = () => {
+    navigate('/about-us');
+  }
   return(
-      <div>
+      <div id='header-home'>
         <Typewriter
           onInit={(typewriter) => {
             typewriter
@@ -16,8 +21,12 @@ function HomePage() {
               .start();
           }}
         />
-
-        <p>This platform was designed for students to meet up with others and study for class they are struggling in</p>
+        <p id='p-home'>This platform was designed for students to meet up with others and study for class they are struggling in</p>
+        <form>
+        <button id='about-us' onClick={navigateToAboutUs} type="submit" className = "btn btn-info" formaction="https://github.com/chystarr/study-buddies">Learn about us</button>
+        <button id='view-git-button' type="submit" className = "btn btn-info" formaction="https://github.com/chystarr/study-buddies">View on Github</button>
+        </form>
+         
       </div>
 
   );
