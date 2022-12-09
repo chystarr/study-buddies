964304faeb69e86a4d82546254431cb542ec9832
chystarr <chynnastarr2@gmail.com>
2022-12-09 06:44:07 -0500

    Profile page now shows user's classes and groups

--- client/src/components/ClassCard.js
+++ client/src/components/ClassCard.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function ClassCard({ id }) {
+function ClassCard({ id, className }) {
   return(
     <div className="card col-10 col-md-8 col-lg-7 mx-auto">
-      <h5 className="card-title">Class Name</h5>
-      <h6 className="card-subtitle mb-2 text-muted">School Name</h6>
+      <h5 className="card-title">{className}</h5>
       <h6 className="card-subtitle mb-2 text-muted">Subject Name</h6>
+      <h6 className="card-subtitle mb-2 text-muted">School Name</h6>
       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <Link to={"/classes/" + id}>Details</Link>
-      <button className="btn btn-primary">Add class</button>
     </div>
   );
   /*
--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,10 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import ErrorAlert from "../components/ErrorAlert";
+import LoadingSpinner from "../components/LoadingSpinner";
+import ClassCard from "../components/ClassCard";
+import GroupCard from "../components/GroupCard";
 
 function ProfilePage() {
+  const [classes, setClasses] = useState([]);
+  const [groups, setGroups] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  let params = useParams();
+  
+  useEffect(() => {
+    async function getData () {
+      try {
+        let classesResponse = await fetch("/api/classes/enrolled");
+        let classesData = await classesResponse.json();
+        setClasses(classesData);
+
+        let groupsResponse = await fetch("/api/groups/joined");
+        let groupsData = await groupsResponse.json();
+        setGroups(groupsData);
+
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching /api/enrolled/ or /api/groups/joined 1", error);
+        setError(true);
+      }
+    }
+    
+    getData();
+
+    return () => {
+      // clean up function
+    };
+  }, [params.id]);
+
+  if (error) {
+    return(
+      <ErrorAlert details={"Error fetching /api/enrolled/ or /api/groups/joined"} />
+    );
+  }
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
   return(
       <div>
         <p>This is the Profile page</p>
         <p>The user's info is displayed here</p>
+        <p>My Classes</p>
+        {classes.map((classData) => (
+          <ClassCard id={classData.id} className={classData.className} key={classData.id} />
+        ))}
+        <p>My Groups</p>
+        {groups.map((groupData) => (
+          <GroupCard id={groupData.id} groupName={groupData.groupName} className={groupData.classId} key={groupData.id} />
+        ))}
       </div>
   );
 }
